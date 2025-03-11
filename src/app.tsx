a282aa123a5dcbe39684b04ba5932a9feab1d957
inharul <88318546+inharul@users.noreply.github.com>
2025-03-12 01:56:01 +0530

    tailwind support

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,10 @@
-import "./index.css"; // import css
-
 import * as React from "react";
 import { createRoot } from "react-dom/client";
+import App from "./app/App";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <h1>Hello react</h1>
+    <App />
   </React.StrictMode>
 );
